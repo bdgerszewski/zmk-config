4d1c0324e0001f6304f6a06330038c59ebd3ba37
Ben <bdgerszewski@gmail.com>
2025-07-03 09:26:34 -0700

    Checkpoint, can build with main zmk

--- localbuild.sh
+++ localbuild.sh
@@ -2,19 +2,21 @@
 
 START_DIR=$(pwd)
 cd /home/ben/repos/zmk-config
-source ~/zephyrproject/.venv/bin/activate
+# source ~/zephyrproject/.venv/bin/activate
 
-west build -s zmk/app -d /home/ben/zmk-dongle-build-mctech-aluminum/left -b nice_nano_v2 -p always --   -DZMK_CONFIG=/home/ben/repos/zmk-config -DSHIELD="corne_left_peripheral nice_view_adapter nice_view_benflix" &
-west build -s zmk/app -d /home/ben/zmk-dongle-build-mctech-aluminum/right -b nice_nano_v2 -p always --  -DZMK_CONFIG=/home/ben/repos/zmk-config -DSHIELD="corne_right nice_view_adapter nice_view_benflix" &
-west build -s zmk/app -d /home/ben/zmk-dongle-build-mctech-aluminum/dongle -b nice_nano_v2 -p always -- -DZMK_CONFIG=/home/ben/repos/zmk-config -DSHIELD="corne_dongle_pro_micro" &
-west build -s zmk/app -d /home/ben/zmk-dongle-build-mctech-aluminum/reset -b nice_nano_v2 -p always --  -DZMK_CONFIG=/home/ben/repos/zmk-config -DSHIELD="settings_reset" &
+west build -s zmk/app -d /home/ben/zmk-dongle-build-mctech-aluminum/left -b nice_nano_v2 -p always --   -DZMK_CONFIG=/home/ben/repos/zmk-config/config -DSHIELD="corne_left_peripheral nice_view_adapter nice_view_benflix" &
+west build -s zmk/app -d /home/ben/zmk-dongle-build-mctech-aluminum/right -b nice_nano_v2 -p always --  -DZMK_CONFIG=/home/ben/repos/zmk-config/config -DSHIELD="corne_right nice_view_adapter nice_view_benflix" &
+west build -s zmk/app -d /home/ben/zmk-dongle-build-mctech-aluminum/dongle -b nice_nano_v2 -p always -- -DZMK_CONFIG=/home/ben/repos/zmk-config/config -DSHIELD="corne_dongle_pro_micro" &
+west build -s zmk/app -d /home/ben/zmk-dongle-build-mctech-aluminum/reset -b nice_nano_v2 -p always --  -DZMK_CONFIG=/home/ben/repos/zmk-config/config -DSHIELD="settings_reset" &
 wait
 
-mkdir -p /home/ben/corne-fw-mctech-aluminum
-cp /home/ben/zmk-dongle-build-mctech-aluminum/left/zephyr/zmk.uf2   /home/ben/corne-fw-mctech-aluminum/corne_left.uf2
-cp /home/ben/zmk-dongle-build-mctech-aluminum/right/zephyr/zmk.uf2  /home/ben/corne-fw-mctech-aluminum/corne_right.uf2
-cp /home/ben/zmk-dongle-build-mctech-aluminum/dongle/zephyr/zmk.uf2 /home/ben/corne-fw-mctech-aluminum/corne_dongle.uf2
-cp /home/ben/zmk-dongle-build-mctech-aluminum/reset/zephyr/zmk.uf2  /home/ben/corne-fw-mctech-aluminum/corne_reset.uf2
+BUILD_OUTPUT_DIR="/mnt/c/Users/skipp/Documents/zmk-local-build-25july"
+mkdir -p $BUILD_OUTPUT_DIR
+# mkdir -p /home/ben/corne-fw-mctech-aluminum
+cp /home/ben/zmk-dongle-build-mctech-aluminum/left/zephyr/zmk.uf2   $BUILD_OUTPUT_DIR/corne_left.uf2
+cp /home/ben/zmk-dongle-build-mctech-aluminum/right/zephyr/zmk.uf2  $BUILD_OUTPUT_DIR/corne_right.uf2
+cp /home/ben/zmk-dongle-build-mctech-aluminum/dongle/zephyr/zmk.uf2 $BUILD_OUTPUT_DIR/corne_dongle.uf2
+cp /home/ben/zmk-dongle-build-mctech-aluminum/reset/zephyr/zmk.uf2  $BUILD_OUTPUT_DIR/corne_reset.uf2
 
 cd $START_DIR
-deactivate
+# deactivate
